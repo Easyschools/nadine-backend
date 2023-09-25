0146d62a0873868a986d48e12ff8abffa7f152d2
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2023-09-25 15:39:38 +0300

    fix: fixed product excel import to deal with calculated values and ensure unique slug.

--- app/Imports/ProductsImport.php
+++ app/Imports/ProductsImport.php
@@ -15,6 +15,23 @@ use App\Traits\HelperFunctions;
 
 class ProductsImport implements ToCollection
 {
+    protected function getCalculatedValue($value, $row)
+    {
+        if (strpos($value, '=') === 0) {
+            $lowercaseAlphabets = [];
+            for ($i = 97; $i <= 122; $i++) {
+                $lowercaseAlphabets[] = chr($i);
+            }
+            $col = array_search(strtolower($value[1]), $lowercaseAlphabets);
+            $postionOfArthematicOperator = strpos($value, '+') ?: strpos($value, '-') ?: strpos($value, '*') ?: strpos($value, '/');
+            $formula = "x" . substr($value, $postionOfArthematicOperator);
+            $formula = str_replace("x", $row[$col], $formula);
+            $value = eval("return $formula;");
+        }
+
+        return round($value, 3);
+    }
+
     public function collection(ExcelCollection $rows)
     {
         $first = true;
@@ -26,66 +43,66 @@ class ProductsImport implements ToCollection
             }
 
             $product = Product::where('id', $row[0])->first();
-            if (!($product->name_en == $row[2] && $product->sku == $row[7])) {
-                $slug = HelperFunctions::makeSlug($row[2]) . '-' . HelperFunctions::makeSlug($row[7]);
-            }
-            $product->update([
-                'name_ar' => $row[1],
-                'name_en' => $row[2],
-                'slug' => $slug ?? $product->slug,
-                'description_ar' => $row[3],
-                'description_en' => $row[4],
-                'price' => $row[5],
-                'price_after_discount' => $row[6],
-                'sku' => $row[7],
-                'collection_id' => Collection::where('name_en', $row[8])->first()?->id,
-                'material_id' => Material::where('name_en', $row[9])->first()?->id,
-                'tag_id' => Tag::where('name_en', $row[10])->first()?->id,
-            ]);
+            $slug = HelperFunctions::makeSlug($row[2] . '-' . rand(100000, 999999));
 
-            $variants = [];
-            $oldVariants = $product->variants()->select('id', 'color_id', 'dimension_id', 'additional_price')->get();
-            for ($i = 11; isset($row[$i]); $i++) {
-                $variant = explode(',', $row[$i]); // name_en, name_ar, image, additional_price, dimension
-                $color = Color::firstOrCreate([
-                    'name_en' => trim($variant[0]),
-                ], [
-                    'name_ar' => trim($variant[1]),
-                    'image' => isset($variant[2]) ? trim($variant[2]) : null,
+            if ($product) {
+                $product->update([
+                    'name_ar' => $row[1],
+                    'name_en' => $row[2],
+                    'slug' => $slug !== $product->slug ? $slug : $product->slug,
+                    'description_ar' => $row[3],
+                    'description_en' => $row[4],
+                    'price' => $this->getCalculatedValue($row[5], $row),
+                    'price_after_discount' => $this->getCalculatedValue($row[6], $row),
+                    'sku' => $row[7],
+                    'collection_id' => Collection::where('name_en', $row[8])->first()?->id,
+                    'material_id' => Material::where('name_en', $row[9])->first()?->id,
+                    'tag_id' => Tag::where('name_en', $row[10])->first()?->id,
                 ]);
 
-                $dimension = Dimension::firstOrCreate([
-                    'dimension' => trim($variant[count($variant) - 1]),
-                ]);
+                $variants = [];
+                $oldVariants = $product->variants()->select('id', 'color_id', 'dimension_id', 'additional_price')->get();
+                for ($i = 11; isset($row[$i]); $i++) {
+                    $variant = explode(',', $row[$i]); // name_en, name_ar, image, additional_price, dimension
+                    $color = Color::firstOrCreate([
+                        'name_en' => trim($variant[0]),
+                    ], [
+                        'name_ar' => trim($variant[1]),
+                        'image' => isset($variant[2]) ? trim($variant[2]) : null,
+                    ]);
 
-                $variants[] = [
-                    'color_id' => $color->id,
-                    'dimension_id' => $dimension->id,
-                    'additional_price' => $variant[3],
-                ];
-            }
+                    $dimension = Dimension::firstOrCreate([
+                        'dimension' => trim($variant[count($variant) - 1]),
+                    ]);
 
-            // dd($oldVariants, $variants);
-            foreach ($oldVariants as $oldVariant) {
-                $isExist = false;
-                foreach ($variants as $key => $variant) {
-                    if ($oldVariant->color_id == $variant['color_id'] && $oldVariant->dimension_id == $variant['dimension_id']) {
-                        $isExist = true;
-                        if ($oldVariant->additional_price != $variant['additional_price']) {
-                            $oldVariant->update(['additional_price' => $variant['additional_price']]);
+                    $variants[] = [
+                        'color_id' => $color->id,
+                        'dimension_id' => $dimension->id,
+                        'additional_price' => $variant[3],
+                    ];
+                }
+
+                foreach ($oldVariants as $oldVariant) {
+                    $isExist = false;
+                    foreach ($variants as $key => $variant) {
+                        if ($oldVariant->color_id == $variant['color_id'] && $oldVariant->dimension_id == $variant['dimension_id']) {
+                            $isExist = true;
+                            if ($oldVariant->additional_price != $variant['additional_price']) {
+                                $oldVariant->update(['additional_price' => $variant['additional_price']]);
+                            }
+                            unset($variants[$key]);
+                            break;
                         }
-                        unset($variants[$key]);
-                        break;
+                    }
+
+                    if (!$isExist) {
+                        $oldVariant->delete();
                     }
                 }
 
-                if (!$isExist) {
-                    $oldVariant->delete();
+                foreach ($variants as $variant) {
+                    $product->variants()->create($variant);
                 }
-            }
-
-            foreach ($variants as $variant) {
-                $product->variants()->create($variant);
             }
         }
     }
